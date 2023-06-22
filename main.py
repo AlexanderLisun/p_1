7dfab02c052c96d934754bf2a1e1f6b4a1a0aadc
Alexandr <alexandrlisun@gmail.com>
2023-06-22 17:17:38 +0200

    initial

--- main.py
+++
@@ -1,8 +0,0 @@
-def strcounter(s):
-    syms_counter = {}
-    for sym in s:
-        syms_counter[sym] = syms_counter.get(sym, 0) + 1
-    for sym, count in syms_counter.items():
-        print(sym, '-', count)
-
-strcounter('asffggce')
